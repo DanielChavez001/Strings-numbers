61fff144dfc4462c07c39d13ca4408c870ba467e
DanielChavez001 <76968877+DanielChavez001@users.noreply.github.com>
2021-03-15 16:37:04 -0700

    Add files via upload

---
+++ numbers.js
@@ -0,0 +1,112 @@
+// Implement the following functions
+
+// function add(a, b) {
+//     // add a to b
+//     return a + b;
+//   }
+//   let result = add(3, 6);
+//   console.log(result);
+
+// function sub(a, b) {
+//     // sub a from b
+//     return a - b;
+//   }
+//   let result = sub(2, 8);
+//   console.log(result);
+
+// function mul(a, b) {
+//     // mul a by b
+//     return a * b;
+//   }
+//   let result = mul(2, 9);
+//   console.log(result);
+
+// function div(a, b) {
+//     // div a by b
+//     return a / b;
+//   }
+//   let result = div(8, 2);
+//   console.log(result);
+
+// function Exponentiation(a, b) {
+//     // expont a of b
+//     return a ** b;
+//   }
+//   let result = Exponentiation(2, 4);
+//   console.log(result)
+
+// function Increment(a, b) {
+//     // inc a by b
+//     let = a++
+//     let = b++
+//     return a, b;
+//   }
+//   let result = Increment(3, 1);
+//   console.log(result)
+
+// function Decrement(a, b) {
+//     // dec a by b
+//     let = a--
+//     let = b--
+//     return a, b;
+//   }
+//   let result = Decrement(3, 2);
+//   console.log(result)
+  
+// function NumberListA(start,end)
+//     {
+//     // return list of numbers starting at start and ending at end
+//     return Array(end - start + 1).fill().map((_, idx) => start + idx)
+// }
+//     let result = NumberListA(1, 15);
+//     console.log(result);
+
+// function NumberListB(start,count, skipEvery)
+//     {
+//     // return list of numbers starting at start and ending at end but skipping every N (ie: skip every 2, or skip every 3)
+//     }
+
+// function NumberListC(start,count, shouldBeRandomOrNot)
+//     {
+//     // return list of numbers starting, continuing for (count) and a boolean to determine if the list of numbers should be random or not
+//     }
+
+// function IsOdd(aNumber)
+//     {
+//     // is a number odd? true false
+//     if (aNumber%2 !== 0)
+// 		return true;
+// 	else
+// 		return false;
+//     }
+//     let result = IsOdd(7)        
+//     console.log(result)
+
+// function IsEven(aNumber)
+//     {
+//     // is a number even? true false
+//     if (aNumber%2 == 0)
+// 		return true;
+// 	else
+// 		return false;
+//     }
+//     let result = IsEven(8)        
+//     console.log(result)
+
+// function RandomNumberList(start,end)
+//     {
+//      // return list of random numbers between start and end
+// }
+//  |
+//  \/
+// let random = [];
+//     while(random.length < 8){
+//     let r = Math.floor(Math.random() * 100) + 1;
+//     if(random.indexOf(r) === -1) random.push(r);
+//     }
+// console.log(random);
+      
+// function RandomNumberList(start,end,count, allowDuplicates)
+//     {
+//     // return count number of random numbers, between start and end. use allowDuplicates to choose if we should allow duplicates in the list or not
+//     }        
---
+++ strings.js
@@ -0,0 +1,82 @@
+// Implement the following functions
+// your code. Refactor this file as you see fit. Export or modify any function as you see fit. Add additonal functions as you see fit. 
+
+// const stringLib = () => 
+//     {
+
+// function concatanate() {
+// // append one string to another
+//     let a = "Hello ";
+//     let b = "world!";
+//     let res = a.concat(b);
+//   }
+
+// function ASCIINumberToChar(Number)
+// {
+// // take one number, say, 127 and convert it to its ASCII char representation
+//     console.log(String.fromCharCode(65));
+// }
+
+    
+// function CharToAsciiNumber(aChar)
+// {
+// // take one Character, say, 'A' and convert it to it's Number representation
+//     let char = 'A';
+//     let asciiCode = char.charCodeAt(0);
+//     console.log(asciiCode);
+// }
+
+
+// function ReverseString(str)
+// {
+// // return a string, ie: Hello becomes olleH
+//     let newString = "";
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         newString += str[i];
+//     }
+//     return newString;
+// }
+// let string = prompt('Hello');
+// let result = ReverseString(string);
+// console.log(result)
+
+
+
+// function GenerateOnlyUpperCase()
+// {
+// //Ie: A,B,C,D,E,F,G...Z
+// let array = ["aaaaa","bbbbb","cccc","dddd","eeee"];
+// array = array.map(function(x){ return x.toUpperCase(); })
+// console.log(array)
+// }
+
+
+// function GenerateOnlyLowerCase()
+// {
+// // Ie: a,b,c,d,e,f,g...z
+// let array = ["AAAAAA","BBBBB","CCCCC","DDDDD","EEEEE"];
+// array = array.map(function(x){ return x.toLowerCase(); })
+// console.log(array)
+// }
+
+
+// function GenerateOnlyNumbers(a, b)
+// {
+// //Ie: 0,1,2,3...9
+//     return Array(b - a + 1).fill().map((_, idx) => a + idx)
+// }
+//     let result = GenerateOnlyNumbers(1, 10);
+//     console.log(result);
+
+function GenerateOnlySymbols()
+{
+//Ie: $, %, !, #, etc
+}
+
+
+function GenerateUpperAndLowerCase()
+{
+ //Ie: A,B,C,D,E,F,G...Z and  a,b,c,d,e,f,g...z
+
+}
+//i have no idea how to do both
